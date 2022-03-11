9062dbb7de392732bb6f2a2f4d4c296de03b807c
Raviraj531 <kuntaraviraj@gmail.com>
2022-03-11 11:51:11 +0530

    1 file modified

--- index.js
+++ index.js
@@ -299,3 +299,21 @@ function reduceExample() {
   console.log('reduce', arr);
 }
 reduceExample();
+
+function sortArray(arr) {
+  var temp = [];
+  if (Array.isArray(arr)) {
+      for (let i = 0; i < arr.length; i++) {
+          for (let j = 0; j < arr.length; j++) {
+              if (arr[i] < arr[j]) {
+                  temp = arr[j];
+                  arr[j] = arr[i];
+                  arr[i] = temp;
+              }
+          }
+      }
+      return arr;
+  }
+}
+
+console.log(sortArray([2, 6, 0, 4, 3, 4, 3, 5, 9, 6, 12, 43, 6]));
